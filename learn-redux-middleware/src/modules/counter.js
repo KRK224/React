58ac2d891c25d51eefe85b2317b636e561d56f16
KRyun <hihas93@naver.com>
2021-08-10 19:03:16 +0900

    Redux-saga example

--- learn-redux-middleware/src/modules/counter.js
+++ learn-redux-middleware/src/modules/counter.js
@@ -1,12 +1,16 @@
 import {createAction, handleActions} from 'redux-actions';
-
+import {delay, put, takeEvery, takeLatest} from 'redux-saga/effects';
 
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
+const INCREASE_ASYNC = 'counter/INCREASE_ASYNC';
+const DECREASE_ASYNC = 'counter/DECREASE_ASYNC';
 
 export const increase= createAction(INCREASE);
 export const decrease = createAction(DECREASE);
 
+
+/* 1. Redux-Thunk code 주석 처리
 // 1초 뒤에 increase 혹은 decrease 함수를 디스패치 함
 export const increaseAsync = ()=>dispatch =>{
   setTimeout(()=>{
@@ -18,7 +22,13 @@ export const decreaseAsync =()=>dispatch=>{
   setTimeout(()=>{
     dispatch(decrease());
   }, 1000);
-};
+}; */
+
+// 2. redux-saga 코드 시작
+
+export const increaseAsync = createAction(INCREASE_ASYNC, ()=> undefined);
+export const decreaseAsync = createAction(DECREASE_ASYNC, ()=> undefined);
+
 
 const initState = 0;
 
