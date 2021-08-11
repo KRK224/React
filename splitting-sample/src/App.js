d804c7098d736722b0066b4cdc6cafaba011c481
KRyun <hihas93@naver.com>
2021-08-11 19:31:52 +0900

    code Spliting_ class형 컴포넌트에 State 사용하기

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,25 +1,55 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+// build시 하나의 main 파일에 병합됨.
+// import notify from './notify';
 
-function App() {
+// shift + alt + a : 코드블록 주석처리
+
+/* function App() {
+  const onClick = () => {
+    // import() 함수 형태로 사용하면, 파일을 따로 분리시켜서 저장.
+    import("./notify").then((result) => result.default());
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p onClick={onClick}>Hello React!</p>
       </header>
     </div>
   );
+} */
+
+// class 형 component;
+
+class App extends Component {
+  state={
+    SplitMe: null
+  };
+
+  handleClick = async() =>{
+    const loadedModule = await import('./SplitMe');
+    this.setState({
+      SplitMe: loadedModule.default
+    });
+  };
+
+  render() {
+    const { SplitMe } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} alt="logo" className="App-logo" />
+          <p onClick={this.handleClick}>Hello React!</p>
+          {SplitMe && <SplitMe />}
+        </header>
+      </div>
+    )
+  }
 }
+
+
 
 export default App;
